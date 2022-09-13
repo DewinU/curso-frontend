847ea03e9a3ab9fa9991f2c0fe70e10a485b3aac
DewinU <umana.dewin@gmail.com>
2022-09-13 13:43:04 -0600

    Migrate from webpack to vite

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,5 @@ import App from './routes/App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
